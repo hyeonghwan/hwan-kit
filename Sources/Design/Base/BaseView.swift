880247f6303cc3dd20c1a04ad4260c946e0d78d3
pHH <gudghks56@gmail.com>
2025-07-21 18:13:48 +0900

    feat: base view + vc

--- Sources/Design/Base/BaseView.swift
+++ Sources/Design/Base/BaseView.swift
@@ -1,8 +1,23 @@
-//
-//  File.swift
-//  hwan-kit
-//
-//  Created by hwan on 7/21/25.
-//
 
-import Foundation
+
+#if canImport(UIKit)
+import UIKit
+
+@MainActor
+open class BaseView: UIView {
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        addChild()
+        addAttributes()
+    }
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        addAttributes()
+    }
+    
+    open func addChild() { }
+    open func addAttributes() { }
+}
+
+#endif
--- Sources/Design/Base/BaseViewController.swift
+++ Sources/Design/Base/BaseViewController.swift
@@ -1,8 +1,19 @@
-//
-//  File.swift
-//  hwan-kit
-//
-//  Created by hwan on 7/21/25.
-//
 
-import Foundation
+#if canImport(UIKit)
+import UIKit
+
+open class BaseViewController: UIViewController {
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        addChild()
+        addAttributes()
+        addLayout()
+        binding()
+    }
+    open func addChild() { }
+    open func addAttributes() { }
+    open func addLayout() { }
+    open func binding() { }
+}
+
+#endif
