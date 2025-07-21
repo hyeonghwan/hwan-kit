8d3bd88a399f66bced2c71ed7819bae6c1832829
pHH <gudghks56@gmail.com>
2025-07-21 18:12:32 +0900

    feat: UILabel extension

--- Sources/Design/Extension/UILabel+.swift
+++ Sources/Design/Extension/UILabel+.swift
@@ -1,8 +1,37 @@
-//
-//  File.swift
-//  hwan-kit
-//
-//  Created by hwan on 7/21/25.
-//
 
-import Foundation
+
+#if canImport(UIKit)
+import UIKit
+
+public extension UILabel {
+    func isTruncated(with constrainedHeight: CGFloat) -> Bool {
+        guard let labelText = self.text else { return false }
+        let requiredSize = CGSize(
+            width: self.frame.width,
+            height: CGFloat.greatestFiniteMagnitude
+        )
+        let attributes: [NSAttributedString.Key: Any] = [.font: self.font!]
+        let requiredRect = labelText.boundingRect(
+            with: requiredSize,
+            options: .usesLineFragmentOrigin,
+            attributes: attributes,
+            context: nil
+        )
+        return requiredRect.height > constrainedHeight
+    }
+    
+    func setLineSpacing(spacing: CGFloat) {
+        guard let text = text else { return }
+        let attributeString = NSMutableAttributedString(string: text)
+        let style = NSMutableParagraphStyle()
+        style.lineSpacing = spacing
+        attributeString.addAttribute(
+            .paragraphStyle,
+            value: style,
+            range: NSRange(location: 0, length: attributeString.length)
+        )
+        attributedText = attributeString
+    }
+}
+
+#endif
