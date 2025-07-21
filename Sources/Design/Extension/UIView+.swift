1796afa892adf2c637d86bd77106e7f4a50dbae6
pHH <gudghks56@gmail.com>
2025-07-21 18:12:27 +0900

    feat: UIView extension

--- Sources/Design/Extension/UIView+.swift
+++ Sources/Design/Extension/UIView+.swift
@@ -1,8 +1,22 @@
-//
-//  File.swift
-//  hwan-kit
-//
-//  Created by hwan on 7/21/25.
-//
 
-import Foundation
+#if canImport(UIKit)
+import UIKit
+
+public extension UIView {
+    var windowWidth: CGFloat {
+        self.window?.windowScene?.screen.bounds.width ?? UIScreen.main.bounds.width
+    }
+    var windowHeight: CGFloat {
+        self.window?.windowScene?.screen.bounds.height ?? UIScreen.main.bounds.height
+    }
+    
+    func loadXib() {
+        let identifier = String(describing: type(of: self))
+        let nibs = Bundle.main.loadNibNamed(identifier, owner: self, options: nil)
+        guard let customView = nibs?.first as? UIView else { return }
+        customView.frame = self.bounds
+        self.addSubview(customView)
+    }
+}
+#endif
+
