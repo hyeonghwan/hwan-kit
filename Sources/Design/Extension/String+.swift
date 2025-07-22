6e9a2d6f19d3fa662df6992a1d26bd9f62d8222e
pHH <gudghks56@gmail.com>
2025-07-22 18:03:28 +0900

    feat: String cacluate Height function add

--- Sources/Design/Extension/String+.swift
+++ Sources/Design/Extension/String+.swift
@@ -30,6 +30,28 @@ public extension String {
         }
         return attributed
     }
+    
+    func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = self.boundingRect(
+            with: constraintRect,
+            options: .usesLineFragmentOrigin,
+            attributes: [.font: font],
+            context: nil
+        )
+        return ceil(boundingBox.height)
+    }
+
+    func width(withConstrainedHeight height: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
+        let boundingBox = self.boundingRect(
+            with: constraintRect,
+            options: .usesLineFragmentOrigin,
+            attributes: [.font: font],
+            context: nil
+        )
+        return ceil(boundingBox.width)
+    }
 }
 
 private extension NSRange {
