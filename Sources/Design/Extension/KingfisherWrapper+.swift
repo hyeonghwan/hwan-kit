71e7ef6936c5644df9c29fbf68ace02b479707eb
pHH <gudghks56@gmail.com>
2025-07-27 14:45:13 +0900

    fix: Public extension

--- Sources/Design/Extension/KingfisherWrapper+.swift
+++ Sources/Design/Extension/KingfisherWrapper+.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Kingfisher
 @MainActor
-extension KingfisherWrapper where Base == KFCrossPlatformImageView {
+public extension KingfisherWrapper where Base == KFCrossPlatformImageView {
     
     mutating func downSizingImage(url: URL, size: CGSize) {
         let processor = DownsamplingImageProcessor(
